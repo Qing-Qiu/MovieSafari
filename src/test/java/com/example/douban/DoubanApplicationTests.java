3cdfb458d5903c707b7ea970a677fdaa46652a69
Qing-Qiu <1223613942@qq.com>
2023-12-22 02:17:26 +0800

    完成分词及统计词频接口

--- src/test/java/com/example/douban/DoubanApplicationTests.java
+++ src/test/java/com/example/douban/DoubanApplicationTests.java
@@ -2,22 +2,33 @@ package com.example.douban;
 
 import com.example.douban.mapper.AccountMapper;
 import com.example.douban.pojo.Account;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.*;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.IDRescorer;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.wltea.analyzer.cfg.DefaultConfig;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @SpringBootTest
@@ -40,9 +51,21 @@ class DoubanApplicationTests {
     public void toTest() throws Exception {
         File modelFile = new File("src/main/resources/static/ratInfo2.txt");
         DataModel model = new FileDataModel(modelFile);
-        ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
-        Recommender recommender = new GenericItemBasedRecommender(model, similarity);
-        List<RecommendedItem> recommendedItemList = recommender.recommend(2, 4);
+        RecommenderBuilder recommenderBuilder = dataModel -> {
+            ItemSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+            return new GenericItemBasedRecommender(dataModel, similarity);
+        };
+        List<RecommendedItem> recommendedItemList = recommenderBuilder.buildRecommender(model).recommend(5, 100);
+        RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+        double score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 1.0);
+        System.out.println(score);
+        RecommenderIRStatsEvaluator statsEvaluator = new GenericRecommenderIRStatsEvaluator();
+        IRStatistics statistics = statsEvaluator.evaluate(recommenderBuilder,
+                null, model, null, 4,
+                GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD,
+                1.0);
+        System.out.println(statistics.getPrecision());
+        System.out.println(statistics.getRecall());
         System.out.println(recommendedItemList.size());
         for (RecommendedItem recommendedItem : recommendedItemList) {
             System.out.println(recommendedItem);
